2e59b24bcab4a4145ee1747e3fd1a18cb956d75b
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-19 19:23:27 -0800

    added controls to snake

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 
+let connection;
 
-const setUpInput = function() {
+const setUpInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +15,22 @@ const setUpInput = function() {
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
+  }
+
+  if (key === '\u0077') {
+    connection.write("Move: up");
+  }
+
+  if (key === '\u0061') {
+    connection.write("Move: left");
+  }
+
+  if (key === '\u0064') {
+    connection.write("Move: right");
+  }
+
+  if (key === '\u0073') {
+    connection.write("Move: down");
   }
 };
 module.exports = {setUpInput};
--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const { connect } = require('./client');
 const { setUpInput } = require('./input');
 console.log("Connecting ...");
-connect();
-setUpInput();
+
+setUpInput(connect());
 
 
