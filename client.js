b54a69fc7d45b5856a48cf566f74244ad356ca73
Christopher Rose <48345024+ChristopherRose13@users.noreply.github.com>
2022-01-19 16:42:56 -0800

    refactored the client connection  to seperate file

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",// IP address here,
+    port: 50541 // PORT number here,
+  });
+  conn.on("connect", () => console.log("successfully connected to server"));
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+  // code that does something
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+
+
+module.exports = {connect:connect};
---
+++ play.js
@@ -0,0 +1,5 @@
+const net = require("net");
+
+let connector = require('./client');
+console.log("Connecting ...");
+connector.connect();
